5fbd59d4b8b69263cb518da8daf8657d21ef39c8
2D\april.cunningham <april.cunningham@slalom.com>
2022-03-18 16:03:29 -0400

    Full game functionality complete

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import { calculateWinner } from '../helpers';
 import Board from './Board';
 
+const style = {
+    width: '200px',
+    margin: '20px auto'
+
+}
+
 const Game = () => {
     const [ board, setBoard ] = useState(Array(9).fill(null))
     const [ xIsNext, setXisNext ] = useState(true)
@@ -23,13 +29,22 @@ const Game = () => {
 
     // }
 
-    // renderMoves = () => {
-        
-    // }
+    const renderMoves = () => ( 
+        <button onClick={() => setBoard(Array(9).fill(null))}>
+        Start Game
+        </button>
+    )
 
 
     return (
+        <>
         <Board squares={board} onClick={handleClick}/>
+        <div style={style}>
+            <p>{winner ? 'Winner: ' + winner : 'Next Move: ' + (xIsNext ? 'X' : 'O')}</p>
+            {renderMoves()}
+        </div>
+        </>
+        
     )
 }
 
